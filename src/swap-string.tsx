1dc2bb082d9678c8354946b2dbb5ec006ce4e3be
JI,XIAOYONG <jixiaoyong1995@gmail.com>
2024-04-21 12:03:36 +0800

    feat: 添加修改内容和预览结果的功能

--- src/swap-string.tsx
+++ src/swap-string.tsx
@@ -1,8 +1,9 @@
-import { Action, ActionPanel, Clipboard, List, showToast, Toast } from "@raycast/api";
+import { Action, ActionPanel, Clipboard, Form, showToast, Toast } from "@raycast/api";
 import { useEffect, useState } from "react";
 
 export default function Command() {
   const [currentClipboard, setCurrentClipboard] = useState("");
+  const regex = /([\w.]+)\s*(==|===)\s*(['"`].*?['"`])/g;
 
   useEffect(() => {
     (async () => {
@@ -14,7 +15,6 @@ export default function Command() {
   const swapVariableAndString = async () => {
     const clipboardContent = (await Clipboard.read()).text;
     setCurrentClipboard(clipboardContent);
-    const regex = /([\w\.]+)\s*(==|===)\s*(['"`].*?['"`])/g;
 
     if (!regex.test(currentClipboard)) {
       const errorMsg = "Invalid clipboard: " + currentClipboard;
@@ -31,18 +31,28 @@ export default function Command() {
     }
   };
 
+  const swapVariableAndStringFunc = function (source: string) {
+    return source.replace(regex, "$3 $2 $1");
+  };
+
   return (
-    <List>
-      <List.Item
-        id="string"
-        title="将剪贴板中字符串移动到等号左侧并插入到当前位置"
-        subtitle="比如 a == 'a'，修改后为：'a' == a"
-        actions={
-          <ActionPanel>
-            <Action title="替换并插入" onAction={swapVariableAndString} />
-          </ActionPanel>
-        }
-      />
-    </List>
+    <Form
+      actions={
+        <ActionPanel>
+          <Action title="替换并插入" onAction={swapVariableAndString} />
+        </ActionPanel>
+      }
+    >
+      <Form.Description text="将剪贴板中字符串移动到等号左侧并插入到当前位置" title=""></Form.Description>
+      <Form.Description text="比如 a == 'a'，修改后为：'a' == a" title=""></Form.Description>
+      <Form.TextArea
+        id="ddd"
+        title="剪贴板内容："
+        value={currentClipboard}
+        onChange={setCurrentClipboard}
+        error={regex.test(currentClipboard) ? "" : "不符合格式要求"}
+      ></Form.TextArea>
+      <Form.Description text={swapVariableAndStringFunc(currentClipboard)} title="替换后："></Form.Description>
+    </Form>
   );
 }
